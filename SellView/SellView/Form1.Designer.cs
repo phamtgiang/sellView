e63b2a1f4778fb713bbbe8b6accec024715c6a21
Phamtruonggiang <truonggiangbkhcm@gmail.com>
2021-05-31 10:47:26 +0700

    Initial

---
+++ SellView/SellView/Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace SellView
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnToi = new System.Windows.Forms.Button();
+            this.btnLui = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cbCust = new System.Windows.Forms.ComboBox();
+            this.txtContact = new System.Windows.Forms.TextBox();
+            this.txtPhoneNo = new System.Windows.Forms.TextBox();
+            this.txtFaxNo = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.grdOrders = new System.Windows.Forms.DataGridView();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.grdOrderDetails = new System.Windows.Forms.DataGridView();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.grdOrders)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.grdOrderDetails)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnToi
+            // 
+            this.btnToi.Location = new System.Drawing.Point(37, 32);
+            this.btnToi.Name = "btnToi";
+            this.btnToi.Size = new System.Drawing.Size(75, 23);
+            this.btnToi.TabIndex = 0;
+            this.btnToi.Text = "Tới";
+            this.btnToi.UseVisualStyleBackColor = true;
+            this.btnToi.Click += new System.EventHandler(this.btnToi_Click);
+            // 
+            // btnLui
+            // 
+            this.btnLui.Location = new System.Drawing.Point(37, 110);
+            this.btnLui.Name = "btnLui";
+            this.btnLui.Size = new System.Drawing.Size(75, 23);
+            this.btnLui.TabIndex = 1;
+            this.btnLui.Text = "Lùi";
+            this.btnLui.UseVisualStyleBackColor = true;
+            this.btnLui.Click += new System.EventHandler(this.btnLui_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(222, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Tên khách hàng :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(222, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Địa chỉ liên hệ :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(222, 98);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Số Phone :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(222, 131);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Số Fax :";
+            // 
+            // cbCust
+            // 
+            this.cbCust.FormattingEnabled = true;
+            this.cbCust.Location = new System.Drawing.Point(359, 32);
+            this.cbCust.Name = "cbCust";
+            this.cbCust.Size = new System.Drawing.Size(403, 21);
+            this.cbCust.TabIndex = 6;
+            // 
+            // txtContact
+            // 
+            this.txtContact.Location = new System.Drawing.Point(359, 65);
+            this.txtContact.Name = "txtContact";
+            this.txtContact.Size = new System.Drawing.Size(403, 20);
+            this.txtContact.TabIndex = 7;
+            // 
+            // txtPhoneNo
+            // 
+            this.txtPhoneNo.Location = new System.Drawing.Point(359, 95);
+            this.txtPhoneNo.Name = "txtPhoneNo";
+            this.txtPhoneNo.Size = new System.Drawing.Size(403, 20);
+            this.txtPhoneNo.TabIndex = 8;
+            // 
+            // txtFaxNo
+            // 
+            this.txtFaxNo.Location = new System.Drawing.Point(359, 128);
+            this.txtFaxNo.Name = "txtFaxNo";
+            this.txtFaxNo.Size = new System.Drawing.Size(403, 20);
+            this.txtFaxNo.TabIndex = 9;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.grdOrders);
+            this.groupBox1.Location = new System.Drawing.Point(37, 174);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(725, 100);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Danh sách các đơn đặt hàng của khách hàng đang được chọn :";
+            // 
+            // grdOrders
+            // 
+            this.grdOrders.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grdOrders.Location = new System.Drawing.Point(6, 19);
+            this.grdOrders.Name = "grdOrders";
+            this.grdOrders.Size = new System.Drawing.Size(713, 75);
+            this.grdOrders.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.grdOrderDetails);
+            this.groupBox2.Location = new System.Drawing.Point(37, 293);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(725, 100);
+            this.groupBox2.TabIndex = 11;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Chi tiết của đơn đặt hàng đang được chọn  :";
+            // 
+            // grdOrderDetails
+            // 
+            this.grdOrderDetails.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grdOrderDetails.Location = new System.Drawing.Point(6, 19);
+            this.grdOrderDetails.Name = "grdOrderDetails";
+            this.grdOrderDetails.Size = new System.Drawing.Size(713, 75);
+            this.grdOrderDetails.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.txtFaxNo);
+            this.Controls.Add(this.txtPhoneNo);
+            this.Controls.Add(this.txtContact);
+            this.Controls.Add(this.cbCust);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnLui);
+            this.Controls.Add(this.btnToi);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.grdOrders)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.grdOrderDetails)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnToi;
+        private System.Windows.Forms.Button btnLui;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox cbCust;
+        private System.Windows.Forms.TextBox txtContact;
+        private System.Windows.Forms.TextBox txtPhoneNo;
+        private System.Windows.Forms.TextBox txtFaxNo;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DataGridView grdOrders;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.DataGridView grdOrderDetails;
+    }
+}
+
---
+++ SellView/SellView/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace SellView
+{
+    public partial class Form1 : Form
+    {
+        //₫ịnh nghĩa các thuộc tính dữ liệu cần dùng
+        private String ConnectionString;
+        private DataViewManager dsView;
+        private DataSet ds;
+        private OleDbConnection cn;
+
+        public Form1()
+        {
+            InitializeComponent();
+            //xây dựng chuỗi ₫ặc tả database cần truy xuất
+            ConnectionString = "Provider=Microsoft.Jet.OLEDB.4.0; Data Source =d:\\NorthWind.mdb; ";
+            //tạo ₫ối tượng Connection ₫ến database
+            cn = new OleDbConnection(ConnectionString);
+            //tạo ₫ối tượng DataSet
+            ds = new DataSet("CustOrders");
+            //tạo ₫ối tượng DataApdater quản lý danh sách các khách hàng
+            OleDbDataAdapter da1 = new OleDbDataAdapter
+             ("SELECT * FROM Customers", cn);
+            //ánh xạ Tablename "Table" tới bảng dữ liệu "Customers"
+            da1.TableMappings.Add("Table", "Customers");
+            //chứa bảng Customers vào Dataset
+            da1.Fill(ds);
+            //tạo ₫ối tượng DataApdater quản lý danh sách các ₫ơn ₫ặt hàng
+            OleDbDataAdapter da2 = new OleDbDataAdapter
+            ("SELECT * FROM Orders", cn);
+            //ánh xạ Tablename "Table" tới bảng dữ liệu "Orders"
+            da2.TableMappings.Add("Table", "Orders");
+            // chứa bảng Orders vào Dataset
+            da2.Fill(ds);
+            //tạo ₫ối tượng DataApdater quản lý danh sách các mặt hàng
+            OleDbDataAdapter da3 = new OleDbDataAdapter
+            ("SELECT * FROM [Order Details]", cn);
+            //ánh xạ Tablename "Table" tới bảng dữ liệu "Orders"
+            da3.TableMappings.Add("Table", "OrderDetails");
+            // chứa bảng [Orders Details] vào Dataset
+            da3.Fill(ds);
+            //thiết lập quan hệ "RelCustOrd" giữa bảng Customers và Orders
+            System.Data.DataRelation relCustOrd;
+            System.Data.DataColumn colMaster1;
+            System.Data.DataColumn colDetail1;
+            colMaster1 = ds.Tables["Customers"].Columns["CustomerID"];
+            colDetail1 = ds.Tables["Orders"].Columns["CustomerID"];
+            relCustOrd = new System.Data.DataRelation
+            ("RelCustOrd", colMaster1, colDetail1);
+            //"add" quan hệ vừa tạo vào dataSet
+            ds.Relations.Add(relCustOrd);
+            //thiết lập quan hệ "relOrdDet" giữa bảng Orders & [Order Details]
+            System.Data.DataRelation relOrdDet;
+            System.Data.DataColumn colMaster2;
+            System.Data.DataColumn colDetail2;
+            colMaster2 = ds.Tables["Orders"].Columns["OrderID"];
+            colDetail2 = ds.Tables["OrderDetails"].Columns["OrderID"];
+            relOrdDet = new DataRelation("RelOrdDet", colMaster2, colDetail2);
+            //"add" quan hệ vừa tạo vào dataSet
+            ds.Relations.Add(relOrdDet);
+            //Xác ₫ịnh DataViewManager của DataSet.
+            dsView = ds.DefaultViewManager;
+            //thiết lập Databinding giữa database với 2 DataGridView
+            grdOrders.DataSource = dsView;
+            grdOrders.DataMember = "Customers.RelCustOrd";
+            grdOrderDetails.DataSource = dsView;
+            grdOrderDetails.DataMember = "Customers.RelCustOrd.RelOrdDet";
+            //thiết lập Databinding giữa database với ComboBox
+            cbCust.DataSource = dsView;
+            cbCust.DisplayMember = "Customers.CompanyName";
+            cbCust.ValueMember = "Customers.CustomerID";
+            //thiết lập Databinding giữa database với 3 Textbox
+            txtContact.DataBindings.Add("Text", dsView, "Customers.ContactName");
+            txtPhoneNo.DataBindings.Add("Text", dsView, "Customers.Phone");
+            txtFaxNo.DataBindings.Add("Text", dsView, "Customers.Fax");
+
+        }
+
+        private void btnToi_Click(object sender, EventArgs e)
+        {
+            CurrencyManager cm = (CurrencyManager)this.BindingContext[dsView, "Customers"];
+            //nếu không phải khách hàng cuối thì tiến tới 1 khách hàng
+            if (cm.Position < cm.Count - 1) cm.Position++;
+        }
+
+        private void btnLui_Click(object sender, EventArgs e)
+        {
+            //nếu không phải khách hàng ₫ầu tiên thì lùi 1 khách hàng
+            if (this.BindingContext[dsView, "Customers"].Position > 0)
+                this.BindingContext[dsView, "Customers"].Position--;
+        }
+
+    }
+}
